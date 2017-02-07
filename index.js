c8d950e662c56b3a384dd4343ee1f62662178f83
Dickow, Chris <cdickow@fusionalliance.com>
2017-02-07 15:18:49 -0500

    Init commit for IoT playground

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app     = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/coffee', function(req, res){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    var data = req.body.Message
+    console.log(req.body);
+    res.send(data);
+
+});
+
+app.listen(process.env.PORT || 8081);
+exports = module.exports = app;
