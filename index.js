07269ed4923970a609add7124653891cf328d1f1
Dickow, Chris <cdickow@fusionalliance.com>
2017-02-07 15:47:30 -0500

    Added logging to subscribe

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.post('/coffee', function(req, res){
     res.setHeader("Access-Control-Allow-Origin", "*");
     var data = req.body.Message
-    console.log(req.body);
+    console.log('********');
+    console.log(req.body.SubscribeURL);
+    console.log('********');
     res.send(data);
-
 });
 
 app.listen(process.env.PORT || 8081);
