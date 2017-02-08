c7704e9cc702e282c402f53c35bd9973989905ab
Dickow, Chris <cdickow@fusionalliance.com>
2017-02-08 12:22:23 -0500

    More logging

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app     = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.text()); // support txt encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/coffee', function(req, res){
     //res.setHeader("Access-Control-Allow-Origin", "*");
     //var data = req.body.Message
     console.log('*-*-*-*-*-*-*-*-');
-    console.log(req.headers);
+    console.log(req.body);
     console.log('*-*-*-*-**-*-*');
     res.status(200).json();
 });
